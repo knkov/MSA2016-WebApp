83b447d2fb175e9db1849c1ba952e199927b7418
knkov <knkovaleva@gmail.com>
2016-09-13 15:39:08 +1200

    basic html added

---
+++ js/main.ts
@@ -0,0 +1,27 @@
+//Upload picture of you
+//Face detect API call to get face ID
+//Upload comparison picture
+//Face detect API call to get face ID
+//create a faceId array list of comparison
+//Face similarity API call and get score back
+//Display score 
+
+/**
+ * Face
+ */
+class Face {
+    face_id:string;
+    face_pic:string; //you or other/family/stranger
+    //id, //age, //gender //you or other
+    //picture
+    constructor(faceID:string) {
+        
+
+    }
+}
+
+class Match{
+
+   
+}
+
